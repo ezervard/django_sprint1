cf1f2861392ab1dd7ec27104bb13cfb8fc49c5e9
ezervard <ezervard@gmail.com>
2024-06-28 20:03:38 +0200

    pep8

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -7,6 +7,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),
-    path('category/<slug:category_posts>/', views.category, 
+    path('category/<slug:category_posts>/', views.category,
          name='category_posts')
 ]
